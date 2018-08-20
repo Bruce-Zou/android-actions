b25cdf6a90ea8d923b25393a0185b2a5c611276f
Bruce Zou <zjianz0917@163.com>
2018-08-20 01:00:53 +0000

    support_minicharger to 0 ; 7 LCD

--- build-android.sh
+++ build-android.sh
@@ -1,17 +1,10 @@
-rm android/out
-rm owl/out
-cd android
-ln -s out-master out
-cd ../owl/
-ln -s out-master out
-cd ..
 ./autobuild.sh config
 ./autobuild.sh u-boot
 ./autobuild.sh kernel
 cd android/
 source build/envsetup.sh
 lunch
-#make -j8
+make -j8
 cd ../
 ./autobuild.sh
  
